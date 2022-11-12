939408276dab62fe8f3110d5b92be8834cdf0358
Emmanuel <emmanueljoe777@gmail.com>
2022-11-12 01:42:33 +0100

    second-commit

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const trig = document.querySelector('.trig')
-const calc2 = document.querySelector('.calc2')
-const calc1 = document.querySelector('.calc1')
-const back = document.querySelector('.back')
-const tab = document.querySelector('.tab')
-const content = document.querySelectorAll('.content')
+const tab = document.querySelector(".tab");
+const contents = Array.from(document.getElementsByClassName("content"));
 
-trig.addEventListener('click', function(){
-    calc2.classList.add('switch')
-    calc1.classList.add('remove')
-})
 
-back.addEventListener('click', function(){
-    calc2.classList.remove('switch')
-    calc1.classList.remove('remove')
-})
+contents.map((content) => {
+  content.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        tab.innerText = "";
+        tab.classList.remove("animate");
+        break;
+      case "←":
+        if (tab.innerText) {
+          tab.innerText = tab.innerText.slice(0, -1);
+        }
+        break;
+      case "=":
+        try {
+          tab.innerText = eval(tab.innerText);
+        } catch {
+          tab.innerText = "ERROR!";
+          tab.classList.add("animate");
+        }
+        break;
+      case "Trig.":
+        tab.innerText = "";
+        break;
+      case "Back":
+        tab.innerText = "";
+        break;
+      default:
+        tab.innerText += e.target.innerText;
+    }
+  });
+});
